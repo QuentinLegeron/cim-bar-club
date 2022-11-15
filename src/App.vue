6e891b724f299adeee76cde4b9433c55f2f264bc
QuentinLegeron <quentin.legeron@gmail.com>
2022-11-15 12:04:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Index from './components/Index.vue'
+</script>
+
+<template>
+  <main>
+    <Index />
+  </main>
+</template>
+
---
+++ src/components/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <h1>Bonjour.</h1>
+    </section>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Index',
+})
+
+</script>
