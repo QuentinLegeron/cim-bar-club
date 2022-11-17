41b08cf51ec5aed391141f8c33cb4aab3334aff7
QuentinLegeron <quentin.legeron@gmail.com>
2022-11-17 23:51:55 +0100

    finish digicode

--- src/components/pages/Digicode.vue
+++ src/components/pages/Digicode.vue
@@ -4,9 +4,12 @@
         <img id="music" src="/src/assets/orateur_white.png" alt="musique" @click="Onplay()">
         <audio id="audio" src="/src/assets/Rebecсa BAR Soundtrack OST Theme _ Cyberpunk_ Edgerunners.mp3"></audio>
 
+        <div id="boxvideo"></div>
+
         <div>
             <p id="code"></p>
         </div>
+
         <div>
             <p @click="NewNumber(0)" class="number zero"></p>
             <p @click="NewNumber(1)" class="number one"></p>
@@ -41,17 +44,40 @@ export default {
 
         NewNumber(chiffre: number) {
             var code = document.getElementById("code")
+            var digicode = document.getElementById("digicode")
             if (code !== null) {
-                if (code.innerHTML.length >= 5) {
-                    if (code.innerHTML == "12345") {
-                        console.log("bravo -> video")
-                    }
-                    else {
-                        code.innerHTML = ""
+                if (code.innerHTML.length <= 4) {
+                    code.innerHTML += chiffre
+                    if (digicode !== null) {
+                        digicode.style.backgroundImage = "url('/src/assets/digicode.png')"
                     }
                 }
-                else {
-                    code.innerHTML += chiffre
+                if (code.innerHTML.length == 5) {
+                    if (code.innerHTML == "45268") {
+                        code.innerHTML = ""
+                        const video = document.createElement('video')
+
+                        // 👇️ Remote file
+                        video.src = '/src/assets/animation_fin.mp4'
+
+                        video.autoplay = true;
+                        video.controls = true;
+                        video.muted = false;
+                        video.width = 1536;
+                        video.height = 721;
+
+                        const boxvideo = document.getElementById('boxvideo')
+                        
+                        if (boxvideo !== null) {
+                            boxvideo.appendChild(video)
+                        }
+                    }
+                    else {
+                        if (digicode !== null) {
+                            digicode.style.backgroundImage = "url('/src/assets/digicode_lueur_rouge.png')"
+                        }
+                        code.innerHTML = ""
+                    }
                 }
             }
         },
@@ -197,72 +223,15 @@ export default {
         border-radius: 10px;
     }
 
-</style>
-
-
-
-
-
-
-<!-- digicode -->
-<!-- <script language="javascript">
- 
-
-var num="0";
-var code=new Array(num,num,num,num,num,num,num,num);
-var pos=0;
-
-for (var f = 0; f < 8; f++) {
-  code[f]="v";
-}
-
-c1=new Image(); c1.src="c1.gif"
-c2=new Image(); c2.src="c2.gif"
-c3=new Image(); c3.src="c3.gif"
-c4=new Image(); c4.src="c4.gif"
-c5=new Image(); c5.src="c5.gif"
-c6=new Image(); c6.src="c6.gif"
-c7=new Image(); c7.src="c7.gif"
-c8=new Image(); c8.src="c8.gif"
-c9=new Image(); c9.src="c9.gif"
-c0=new Image(); c0.src="c0.gif"
-cv=new Image(); cv.src="cv.gif"
-
-function affiche(){
-  document.images.a.src=eval("c"+code[0]+".src");
-  document.images.b.src=eval("c"+code[1]+".src");
-  document.images.c.src=eval("c"+code[2]+".src");
-  document.images.d.src=eval("c"+code[3]+".src");
-  document.images.e.src=eval("c"+code[4]+".src");
-  document.images.f.src=eval("c"+code[5]+".src");
-  document.images.g.src=eval("c"+code[6]+".src");
-  document.images.h.src=eval("c"+code[7]+".src");
-}
-function nouveau(num) {
-  if (pos<8) {
-    for (var f = 1; f < 8; f++) {
-      code[f-1]=code[f];
+    video {
+        position: fixed;
+        z-index: 100;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
     }
-    code[7]=eval(num);
-    pos+=1;
-  }
-  affiche();
-}
-function ok() {
-  password="";
-  for (var f = 0; f < 8; f++) {
-    if (code[f]!="v") {password+=code[f]};
-  }
-  var locate;
-  locate = password + ".htm";
-  this.location.href = locate;
-}
-function cancel() {
-  for (var f = 0; f < 8; f++) {
-    code[f]="v";
-  }
-  pos = 0;
-  affiche();
-}
-</script>
-  -->
+
+</style>
